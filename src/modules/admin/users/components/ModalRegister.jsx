0b49abf04680fda1696febf3b42732981c7b18e5
Agles <20223tn005@utez.edu.mx>
2024-03-08 13:36:43 -0600

    Configuracion de yup2

--- src/modules/admin/users/components/ModalRegister.jsx
+++ src/modules/admin/users/components/ModalRegister.jsx
@@ -1,11 +1,44 @@
 import { Button, Label, TextInput, Modal, FileInput, Card } from 'flowbite-react';
+import { Formik, useFormik } from 'formik';
 import React, { useState } from 'react';
+import * as yup from "yup";
 
 const ModalRegister = ({ open, onClose }) => {
     const [openModal, setOpenModal] = useState(false);
     const [modalPlacement, setModalPlacement] = useState('center')
 
+    const formik = useFormik ({
+        initialValues:{
+            username:"",
+            password:"",
+            confirmPassword:"",
+            role:[],
+            name:"",
+            surname:"",
+            lastname:'',
+            curp:'',
+            birthdate:'',
+            avatar:''
+        },
+        validationSchema: yup.object().shape({
+            username:yup.string().required("Campo obligatorio").max(45,"Solo se permiten 45 caracteres").min(4,"minimo 4 caracteres"),
+            password:yup.string().required("Campo obligatorio").min(8,"Minimo 8 caracteres").max(50,"Solo se permiten 50 caracteres"),
+            confirmPassword:yup.string().test("confirm password",
+            "las contraseñas no coindicen ",function (value){return this.parent.password === value;}),
+            role:yup.string().required("Campo obligatorio"),
+            name:yup.string().required("Campo obligatorio").min(3,"Minimo 3 caracteres").max(50,"maximo 50 caracteres"),
+            surname:yup.string().required("Campo obligatorio").min(3,"Minimo 3 caracteres").max(50,"Máximo 50 caracteres"),
+            curp:yup.string().required("Campo obligatorio").min(18,"Minimo 18 caracteres").max(18,"Máximo 18 caracteres"),
+            birthdate:yup.string().required("Campo obligatorio"),
+        }),
+        onSubmit: async (values,{seSubmitting}) =>{
+            console.log(values);
+        },
+    })
+
     return (
+
+
         <>
             <Modal
                 show={open}
@@ -24,33 +57,33 @@ const ModalRegister = ({ open, onClose }) => {
                                     </div>
                                     <div className='mt-2'>
                                         <div className="mb-2 block w-full">
-                                            <Label htmlFor="name1" value="nombre" />
+                                            <Label htmlFor="name" value="nombre" />
                                         </div>
-                                        <TextInput id="name1" name='name1' type="text" placeholder="name" required />
+                                        <TextInput id="name" name='name' type="text" placeholder="name" required />
                                     </div>
                                     <div className='mt-2'>
                                         <div className="mb-2 block">
-                                            <Label htmlFor="lastname1" value="apellido" />
+                                            <Label htmlFor="lastname" value="apellido" />
                                         </div>
-                                        <TextInput id="lastname1" name='lastname1' type="text" placeholder="last name" required />
+                                        <TextInput id="lastname" name='lastname' type="text" placeholder="last name" required />
                                     </div>
                                     <div className='mt-2'>
                                         <div className="mb-2 block">
-                                            <Label htmlFor="surname1" value="segundo apellido" />
+                                            <Label htmlFor="surname" value="segundo apellido" />
                                         </div>
-                                        <TextInput id="surname1" name='surname' type="text" placeholder="surname" required />
+                                        <TextInput id="surname" name='surname' type="text" placeholder="surname" required />
                                     </div>
                                     <div>
                                         <div className="mb-2 block">
-                                            <Label htmlFor="curp1" value="curp" />
+                                            <Label htmlFor="curp" value="curp" />
                                         </div>
-                                        <TextInput id="curp1" type="text" name='curp1' placeholder='curp' required />
+                                        <TextInput id="curp" type="text" name='curp' placeholder='curp' required />
                                     </div>
                                     <div>
                                         <div className="mb-2 block">
-                                            <Label htmlFor="date1" value="birdthdate" />
+                                            <Label htmlFor="date" value="birdthdate" />
                                         </div>
-                                        <TextInput id="date1" type="date" name='curp1' placeholder='curp' required />
+                                        <TextInput id="date" type="date" name='curp1' placeholder='curp' required />
                                     </div>
 
                                 </Card>
@@ -62,21 +95,21 @@ const ModalRegister = ({ open, onClose }) => {
                                     </div>
                                     <div className='mt-3'>
                                         <div className="mb-2 block">
-                                            <Label htmlFor="nameuser1" value="user name" />
+                                            <Label htmlFor="username" value="user name" />
                                         </div>
-                                        <TextInput id="nameuser1" name='username1' type="text" placeholder='name user' required />
+                                        <TextInput id="username" name='username' type="text" placeholder='name user' required />
                                     </div>
                                     <div className='mt-3'>
                                         <div className="mb-2 block">
-                                            <Label htmlFor="rol1" value="Rol" />
+                                            <Label htmlFor="rol" value="Rol" />
                                         </div>
-                                        <TextInput id="rol1" name='rol1' type="select" placeholder='AdminHome' required />
+                                        <TextInput id="rol" name='rol' type="select" placeholder='AdminHome' required />
                                     </div>
                                     <div>
                                         <div className="mb-2 block">
-                                            <Label htmlFor="password1" value="password" />
+                                            <Label htmlFor="password" value="password" />
                                         </div>
-                                        <TextInput id="password1" name='password1' type="text" placeholder='password' required />
+                                        <TextInput id="password" name='password' type="text" placeholder='password' required />
                                     </div>
                                     <div>
                                         <div className="mb-2 block">
